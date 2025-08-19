4108cd163c51beb699aee9a3c20b477a32e9ac43
Deepak9370 <deepaksurykantrathod@gmail.com>
2025-08-19 23:26:20 +0530

    Database Model Created

---
+++ SmartKartProject/PhoneHub/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class PhoneModel(models.Model):
+    brand = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    camera = models.CharField(max_length=50)
+    ram = models.CharField(max_length=50)
+    storage = models.CharField(max_length=50)
+    price = models.FloatField()
